0c77c3af0603780d15efb15dbf390cdaa764d396
rajath U Shetty <rajathushetty56@gmail.com>
2024-12-29 20:47:25 +0530

    fix sign-in

--- src/lib/auth/utils.ts
+++ src/lib/auth/utils.ts
@@ -4,21 +4,11 @@ import GoogleProvider from "next-auth/providers/google";
 import { redirect } from "next/navigation";
 import { db } from "../db";
 
-import type { User } from 'next-auth'
-
 type UserId = string
 
 declare module 'next-auth/jwt' {
   interface JWT {
     id: UserId
-  }
-}
-
-declare module 'next-auth' {
-  interface Session {
-    user: User & {
-      id: UserId
-    }
   }
 }
 
