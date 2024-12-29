acf16917caa4ed12ddf2b096a69e983bc874bca1
rajath U Shetty <rajathushetty56@gmail.com>
2024-12-29 19:15:54 +0530

    changed default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         
-<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>{children}</ThemeProvider>
+<ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>{children}</ThemeProvider>
 
       </body>
     </html>
